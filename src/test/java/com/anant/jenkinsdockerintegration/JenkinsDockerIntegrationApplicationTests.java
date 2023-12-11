f3a3d30a50d2dbb9b162ee65dfd130ac97b36a05
Anant Kumar <anant.kumar@vin.net>
2023-12-11 17:48:56 +0530

    docker-jenkin-integration

---
+++ src/test/java/com/anant/jenkinsdockerintegration/JenkinsDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anant.jenkinsdockerintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
